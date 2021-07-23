7e77f24b44c4e5ec4360eef6bec88ce757c6695b
Yury <Yura388@yandex.ru>
2021-07-23 22:24:04 +0300

    addi 2nd task

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-First task 
+// First  
 
 function createCb(str){
   return function(){
@@ -18,3 +18,26 @@ if(x>10){
 }
 
 foo(16, createCb('someStr'))
+
+
+// Second
+
+function parseJSON(jsonStr, successCB, failureCB){
+  try{
+    let result = JSON.parse(jsonStr)
+    successCB(result)
+  }catch(e){
+    failureCB(e)
+  }
+  
+}
+
+function failureCB(error){
+  console.log('unsuccsess', error)
+}
+
+function successCB(JSONresult){
+  console.log('sucsessParse', JSONresult)
+}
+
+parseJSON('{"x:10}', successCB, failureCB)
